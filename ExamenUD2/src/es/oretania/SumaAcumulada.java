cb315dfdb4425e5bd4ee79c21052b7d61a7df5a3
estudiante <estudiante@DAW1-cbcb25>
2025-11-07 09:59:33 +0100

    Versión 3

--- ExamenUD2/src/es/oretania/SumaAcumulada.java
+++ ExamenUD2/src/es/oretania/SumaAcumulada.java
@@ -5,7 +5,8 @@ public class SumaAcumulada {
 	public static void main(String[] args) {
 		
 		try {
-			int [] numeros = { 10, 5, -2, 2, 1, 5, 0, 7, -1, 3};
+			int [] numeros = { 10, 5, -2, 2, 1, 5, 1, 7, -1, 3};
+			// cambiamos el 0 por otro numero en mi caso un 1
 			int sumaTotal;
 			sumaTotal = calcularSumaDivisiones(numeros);
 			System.out.println("Suma total de las divisiones: " + sumaTotal);
@@ -15,7 +16,9 @@ public class SumaAcumulada {
 	
 	public static int calcularSumaDivisiones(int[] numeros) throws Exception{
 		int suma = 0;
-		for (int i = 1; i <= numeros.length; i ++) {
+		for (int i = 0; i >= numeros.length; i ++) {
+			//iniciamos la variable i = 0 para que use también el 10 en la suma
+			//cambiamos i <= numeros.length por i >=numeros.length
 			int resultado = 100 / numeros[i];
 			suma += resultado;
 		}
@@ -23,3 +26,5 @@ public class SumaAcumulada {
 	}
 
 }
+
+//no me sale el resultado de las divisiones pero si el mensaje
